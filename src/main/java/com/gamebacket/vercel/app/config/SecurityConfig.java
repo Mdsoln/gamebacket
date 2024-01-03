5fb8db5454481756af44bcbb2891c285850fadb7
MUDDY FAKIH <muddyfakih98@gmail.com>
2024-01-03 22:28:07 +0300

    security configurations

--- src/main/java/com/gamebacket/vercel/app/config/SecurityConfig.java
+++ src/main/java/com/gamebacket/vercel/app/config/SecurityConfig.java
@@ -1,2 +1,54 @@
-package com.gamebacket.vercel.app.config;public class SecurityConfig {
+package com.gamebacket.vercel.app.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    public UserDetailsService userDetailsService;
+    @Bean
+    public static PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(10);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests((authorise->
+                        authorise
+                                .requestMatchers("/api/v1/base/**")
+                                .permitAll()
+                                .requestMatchers("/api/v1/base/createAccount")
+                                .permitAll()
+                        )).formLogin(
+                                form->form
+                                        .loginPage("/login")
+                                        .loginProcessingUrl("/login")
+                                        .defaultSuccessUrl("/home")
+                                        .permitAll()
+                ).logout(
+                        logout->logout
+                                .invalidateHttpSession(true)
+                                .clearAuthentication(true)
+                                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                                .logoutSuccessUrl("/login?logout")
+                                .permitAll()
+                );
+
+        return http.build();
+    }
+
+
 }
