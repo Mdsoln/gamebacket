fd0622e5a323d0f3454ffca641b1576c9b50f867
MUDDY FAKIH <muddyfakih98@gmail.com>
2024-01-03 15:29:08 +0300

    initial project setups

---
+++ src/main/java/com/gamebacket/vercel/app/GamebacketApplication.java
@@ -0,0 +1,13 @@
+package com.gamebacket.vercel.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamebacketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamebacketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gamebacket/vercel/app/GamebacketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamebacket.vercel.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamebacketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
