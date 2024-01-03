d663199803386dafcddb61c931f9837896c728c0
MUDDY FAKIH <muddyfakih98@gmail.com>
2024-01-03 22:26:52 +0300

    introducing interfaces for databases

--- src/main/java/com/gamebacket/vercel/app/repo/ContactRepo.java
+++ src/main/java/com/gamebacket/vercel/app/repo/ContactRepo.java
@@ -1,2 +1,10 @@
-package com.gamebacket.vercel.app.repo;public interface ContactRepo {
+package com.gamebacket.vercel.app.repo;
+
+import com.gamebacket.vercel.app.entity.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContactRepo extends JpaRepository<Contact,Long> {
+
 }
--- src/main/java/com/gamebacket/vercel/app/repo/UserRepo.java
+++ src/main/java/com/gamebacket/vercel/app/repo/UserRepo.java
@@ -1,2 +1,10 @@
-package com.gamebacket.vercel.app.repo;public interface UserRepo {
+package com.gamebacket.vercel.app.repo;
+
+import com.gamebacket.vercel.app.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<Customer,Long> {
+
 }
