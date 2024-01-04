0ac764b8fe2a3b457d15167e7825a546cb6d9e7b
MUDDY FAKIH <muddyfakih98@gmail.com>
2024-01-04 22:31:19 +0300

    adding accessories class

--- src/main/java/com/gamebacket/vercel/app/entity/Accessories.java
+++ src/main/java/com/gamebacket/vercel/app/entity/Accessories.java
@@ -1,2 +1,20 @@
-package com.gamebacket.vercel.app.entity;public class Accessories {
+package com.gamebacket.vercel.app.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "accessory")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class Accessories {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id",nullable = false)
+    private Long accessoryId;
 }
