cfed2ddfe0ba016c63dd3b4936805179e0711044
MUDDY FAKIH <muddyfakih98@gmail.com>
2024-01-05 00:51:41 +0300

    client payload request dto

--- src/main/java/com/gamebacket/vercel/app/dto/AccessoryRequest.java
+++ src/main/java/com/gamebacket/vercel/app/dto/AccessoryRequest.java
@@ -1,2 +1,20 @@
-package com.gamebacket.vercel.app.dto;public class AccessoryRequest {
+package com.gamebacket.vercel.app.dto;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class AccessoryRequest {
+    private String productName;
+    private String category;
+    private float price;
+    private String description;
+    private int quantity;
+
+    @Column(name = "image",nullable = false,columnDefinition = "BLOB")
+    private byte[] image;
 }
--- src/main/java/com/gamebacket/vercel/app/dto/GamePublisher.java
+++ src/main/java/com/gamebacket/vercel/app/dto/GamePublisher.java
@@ -1,2 +1,29 @@
-package com.gamebacket.vercel.app.dto;public class GamePublisher {
+package com.gamebacket.vercel.app.dto;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class GamePublisher {
+    private String gameTitle;
+    private String gamePlatforms;
+    private float ActualPrice;
+    private float DiscountPrice;
+    private int gamePlaytime;
+    private int gameAge;
+    private String genre;
+    private String website;
+    private String tags;
+    private String aboutGame;
+    private String requirements;
+    private String releaseDate;
+    private int gameQuantity;
+    private String gameCategory;
+
+    @Column(columnDefinition = "BLOB")
+    private byte[] image;
 }
