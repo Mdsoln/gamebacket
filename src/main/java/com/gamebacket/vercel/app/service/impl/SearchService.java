0e248ee2b3fa5f8d79e67d91e23c1bd4c90e7e5a
MUDDY FAKIH <muddyfakih98@gmail.com>
2024-01-06 23:53:32 +0300

    interface for searching interactions with database for games by name

--- src/main/java/com/gamebacket/vercel/app/service/impl/SearchService.java
+++ src/main/java/com/gamebacket/vercel/app/service/impl/SearchService.java
@@ -1,2 +1,36 @@
-package com.gamebacket.vercel.app.service.impl;public class SearchService {
+package com.gamebacket.vercel.app.service.impl;
+
+import com.gamebacket.vercel.app.entity.Games;
+import com.gamebacket.vercel.app.repo.AccessoriesRepo;
+import com.gamebacket.vercel.app.repo.GameRepo;
+import com.gamebacket.vercel.app.service.inter.SearchInterface;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class SearchService implements SearchInterface {
+    private final GameRepo gameRepo;
+    private final AccessoriesRepo accessoriesRepo;
+
+    @Override
+    public List<Games> findByGameTitleContainingIgnoreCase(String queryGames) {
+        try {
+            return gameRepo.findByGameTitleContainingIgnoreCase(queryGames);
+        }catch (DataAccessException exception){
+            throw new RuntimeException("An error occurred while executing the query.",exception);
+        }
+    }
+
+    @Override
+    public List<String> findAllGamesTitle() {
+        try {
+            return gameRepo.findAllGamesTitle();
+        }catch (DataAccessException exception){
+            throw new RuntimeException("An error occurred while executing the query.",exception);
+        }
+    }
 }
--- src/main/java/com/gamebacket/vercel/app/service/inter/SearchInterface.java
+++ src/main/java/com/gamebacket/vercel/app/service/inter/SearchInterface.java
@@ -1,2 +1,12 @@
-package com.gamebacket.vercel.app.service.inter;public class SearchInterface {
+package com.gamebacket.vercel.app.service.inter;
+
+import com.gamebacket.vercel.app.entity.Games;
+
+import java.util.List;
+
+public interface SearchInterface {
+    List<Games> findByGameTitleContainingIgnoreCase(String queryGames);
+
+    List<String> findAllGamesTitle();
+
 }
