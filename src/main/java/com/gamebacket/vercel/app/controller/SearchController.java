d47cb738e4171a74fc168523fba261965d310127
MUDDY FAKIH <muddyfakih98@gmail.com>
2024-01-06 23:51:18 +0300

    autocomplete suggestions api

--- src/main/java/com/gamebacket/vercel/app/controller/SearchController.java
+++ src/main/java/com/gamebacket/vercel/app/controller/SearchController.java
@@ -1,2 +1,39 @@
-package com.gamebacket.vercel.app.controller;public class SearchController {
+package com.gamebacket.vercel.app.controller;
+
+import com.gamebacket.vercel.app.entity.Games;
+import com.gamebacket.vercel.app.service.impl.SearchService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api/v1/search")
+@RequiredArgsConstructor
+public class SearchController {
+    private final SearchService searchService;
+
+    /**
+     * searching games based on user input and returning all games that contain user input
+     */
+    @GetMapping("/games")
+    public ResponseEntity<List<Games>> searchGameByName(@RequestParam String queryGames){
+        List<Games> matchingGames = searchService.findByGameTitleContainingIgnoreCase(queryGames);
+        return ResponseEntity.ok(matchingGames);
+    }
+
+    /**
+     * Autocomplete search suggestions
+     */
+    @GetMapping("/game-options")
+    public ResponseEntity<List<String>> gameGameOptions(){
+        List<String> gameOptions = searchService.findAllGamesTitle();
+        return ResponseEntity.ok(gameOptions);
+    }
+
+
 }
