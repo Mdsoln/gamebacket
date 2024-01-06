a5d3bd3cf7b4bff61a2c25fb7924d65458f7954e
MUDDY FAKIH <muddyfakih98@gmail.com>
2024-01-06 23:57:17 +0300

    customised querying methods

--- src/main/java/com/gamebacket/vercel/app/repo/GameRepo.java
+++ src/main/java/com/gamebacket/vercel/app/repo/GameRepo.java
@@ -2,8 +2,15 @@ package com.gamebacket.vercel.app.repo;
 
 import com.gamebacket.vercel.app.entity.Games;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface GameRepo extends JpaRepository<Games,Long> {
+    List<Games> findByGameTitleContainingIgnoreCase(String queryGames);
+
+    @Query("SELECT DISTINCT g.gameTitle FROM Games g")
+    List<String> findAllGamesTitle();
 }
