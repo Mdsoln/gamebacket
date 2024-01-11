54215c9e7c11ad155f82493d839648d1e5c68e62
MUDDY FAKIH <muddyfakih98@gmail.com>
2024-01-11 15:13:06 +0300

    setting constants statuses of the system

--- src/main/java/com/gamebacket/vercel/app/constants/Status.java
+++ src/main/java/com/gamebacket/vercel/app/constants/Status.java
@@ -1,7 +1,12 @@
 package com.gamebacket.vercel.app.constants;
 
-public enum Status {
-    completed,
-    ongoing,
-    canceled
+public class Status {
+//  statuses
+    public static final String STATUS_ONGOING = "ongoing";
+    public static final String STATUS_COMPLETED = "completed";
+    public static final String STATUS_CANCELED = "canceled";
+
+//    actions
+    public static final String ACTION_COMPLETE = "complete";
+    public static final String ACTION_CANCEL = "cancel";
 }
