851f2d784ac481178e82bc3bd31fb72bbe54ae98
MUDDY FAKIH <muddyfakih98@gmail.com>
2024-01-12 22:10:56 +0300

    payload for the add to cart functionality

--- src/main/java/com/gamebacket/vercel/app/dto/OrderRequest.java
+++ src/main/java/com/gamebacket/vercel/app/dto/OrderRequest.java
@@ -1,2 +1,16 @@
-package com.gamebacket.vercel.app.dto;public class OrderRequest {
+package com.gamebacket.vercel.app.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class OrderRequest {
+//    for product details
+    private String productName;
+    private int productQuantity;
 }
