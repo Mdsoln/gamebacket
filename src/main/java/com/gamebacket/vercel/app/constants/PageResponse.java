a187a162c12050800b157a0d492570e9d197814e
MUDDY FAKIH <muddyfakih98@gmail.com>
2024-01-13 22:40:28 +0300

    previous and next button for paginated searches

--- src/main/java/com/gamebacket/vercel/app/constants/PageResponse.java
+++ src/main/java/com/gamebacket/vercel/app/constants/PageResponse.java
@@ -1,2 +1,19 @@
-package com.gamebacket.vercel.app.constants;public class PageResponse {
+package com.gamebacket.vercel.app.constants;
+
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class PageResponse<T> {
+    private final List<T> content;
+    private final int totalPages;
+    private final long totalElements;
+
+    public PageResponse(List<T> content, int totalPages, long totalElements, long elements) {
+        this.content = content;
+        this.totalPages = totalPages;
+        this.totalElements = totalElements;
+    }
 }
