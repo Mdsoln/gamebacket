023626147eba3863426e5a499268e3ef7d289463
MUDDY FAKIH <muddyfakih98@gmail.com>
2024-03-03 00:51:16 +0300

    processing payment and retun response accordingly

--- src/main/java/com/gamebacket/vercel/app/service/impl/PaymentFacade.java
+++ src/main/java/com/gamebacket/vercel/app/service/impl/PaymentFacade.java
@@ -1,2 +1,24 @@
-package com.gamebacket.vercel.app.service.impl;public class PaymentFacade {
+package com.gamebacket.vercel.app.service.impl;
+
+import com.gamebacket.vercel.app.constants.PaymentProviderType;
+import com.gamebacket.vercel.app.dto.PaymentRequest;
+import com.gamebacket.vercel.app.dto.PaymentResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class PaymentFacade {
+    private final AirtelMoneyServiceImpl airtelMoneyService;
+    private final TigoPesaServiceImpl tigoPesaService;
+    private final GePGServiceImpl gePGService;
+
+    public PaymentResponse pay(PaymentRequest request){
+        PaymentProviderType providerType = request.getProviderType();
+        return switch (providerType) {
+            case GEPG -> gePGService.pay(request);
+            case TIGO_PESA -> tigoPesaService.pay(request);
+            case AIRTEL_MONEY -> airtelMoneyService.pay(request);
+        };
+    }
 }
