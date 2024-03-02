16c6f1f314e60f7e00d128729a7f40550b10f4d7
MUDDY FAKIH <muddyfakih98@gmail.com>
2024-03-03 00:48:24 +0300

    creating objects for payment request and its response from gateway provider

--- src/main/java/com/gamebacket/vercel/app/dto/PaymentRequest.java
+++ src/main/java/com/gamebacket/vercel/app/dto/PaymentRequest.java
@@ -1,2 +1,15 @@
-package com.gamebacket.vercel.app.dto;public class PaymentRequest {
+package com.gamebacket.vercel.app.dto;
+
+import com.gamebacket.vercel.app.constants.PaymentProviderType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class PaymentRequest {
+    private PaymentProviderType providerType;
+    private String paymentId;
+    private double amount;
 }
--- src/main/java/com/gamebacket/vercel/app/dto/PaymentResponse.java
+++ src/main/java/com/gamebacket/vercel/app/dto/PaymentResponse.java
@@ -1,2 +1,17 @@
-package com.gamebacket.vercel.app.dto;public class PaymentResponse {
+package com.gamebacket.vercel.app.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class PaymentResponse {
+     private boolean success;
+     private String message;
+     private String transactionalID;
+     private Map<String,String> additionalData;
 }
