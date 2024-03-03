a99dc4dcf13f92d7cc0402e7b7198a3e1dd0f1ed
MUDDY FAKIH <muddyfakih98@gmail.com>
2024-03-03 17:40:26 +0300

    adding service for sending email notification to suppliers for every order placed

---
+++ src/main/java/com/gamebacket/vercel/app/config/MailConfiguration.java
@@ -0,0 +1,29 @@
+package com.gamebacket.vercel.app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.stereotype.Service;
+
+import java.util.Properties;
+
+@Service
+public class MailConfiguration {
+
+    @Bean
+    public JavaMailSender javaMailSender(){
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+        javaMailSender.setHost("smtp.gmail.com");
+        javaMailSender.setPort(587);
+        javaMailSender.setUsername("gamebacket@gmail.com");
+        javaMailSender.setPassword("********************");
+
+        Properties properties = javaMailSender.getJavaMailProperties();
+        properties.put("mail.transport.protocol", "smtp");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.debug", "true");
+
+        return javaMailSender;
+    }
+}
---
+++ src/main/java/com/gamebacket/vercel/app/service/impl/EmailService.java
@@ -0,0 +1,30 @@
+package com.gamebacket.vercel.app.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+    private final JavaMailSender javaMailSender;
+
+    private static final String supplierEmail = "muddyfakih98@gmail.com";
+    public void sendMailToSuppliers(String customerEmail, String productName){
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setTo(supplierEmail);
+        simpleMailMessage.setSubject("New Order from Gamebacket");
+        simpleMailMessage.setText(buildNotificationMessage(customerEmail,productName));
+        javaMailSender.send(simpleMailMessage);
+    }
+
+    private String buildNotificationMessage(String customerEmail, String productName) {
+        StringBuilder message = new StringBuilder();
+        message.append("A new order has been placed on Gamebacket.\n");
+        message.append("Customer Email: ").append(customerEmail).append("\n");
+        message.append("Product Name: ").append(productName).append("\n");
+
+        return message.toString();
+    }
+}
