8e05f4eacae2e43062cc2fb1c29cbc1f6e96e252
MUDDY FAKIH <muddyfakih98@gmail.com>
2024-03-05 22:42:26 +0300

    changing return type to AuthenticationResponse for createAccount method inorder to generate token

--- src/main/java/com/gamebacket/vercel/app/service/inter/BaseInterface.java
+++ src/main/java/com/gamebacket/vercel/app/service/inter/BaseInterface.java
@@ -1,10 +1,13 @@
 package com.gamebacket.vercel.app.service.inter;
 
+import com.gamebacket.vercel.app.dto.AuthenticationRequest;
+import com.gamebacket.vercel.app.dto.AuthenticationResponse;
 import com.gamebacket.vercel.app.dto.User;
 
 public interface BaseInterface {
-    void createNewAccount(User user);
+    AuthenticationResponse createNewAccount(User user);
 
     void deleteUserById(Long userId);
 
+    AuthenticationResponse authenticate(AuthenticationRequest request);
 }
