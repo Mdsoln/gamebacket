7f4db48094c78c725e8f4be7f769903d7aefe4d2
MUDDY FAKIH <muddyfakih98@gmail.com>
2024-03-06 01:32:53 +0300

    generating a type dto return type for query top selling product

--- src/main/java/com/gamebacket/vercel/app/dto/TopSellingProductDTO.java
+++ src/main/java/com/gamebacket/vercel/app/dto/TopSellingProductDTO.java
@@ -1,2 +1,15 @@
-package com.gamebacket.vercel.app.dto;public class TopSellingProductDTO {
+package com.gamebacket.vercel.app.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class TopSellingProductDTO {
+    private String productName;
+    private float price;
+    private String image;
+    private int orderCount;
 }
