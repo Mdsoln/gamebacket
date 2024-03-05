430d7864c731bb0b0ba627c7dc4ece8fc9657da8
MUDDY FAKIH <muddyfakih98@gmail.com>
2024-03-05 22:36:13 +0300

    generating token, request for users with username and password to be authenticated

--- src/main/java/com/gamebacket/vercel/app/dto/AuthenticationRequest.java
+++ src/main/java/com/gamebacket/vercel/app/dto/AuthenticationRequest.java
@@ -1,2 +1,15 @@
-package com.gamebacket.vercel.app.dto;public class AuthenticationRequest {
+package com.gamebacket.vercel.app.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class AuthenticationRequest {
+    private String email;
+    private String password;
 }
--- src/main/java/com/gamebacket/vercel/app/dto/AuthenticationResponse.java
+++ src/main/java/com/gamebacket/vercel/app/dto/AuthenticationResponse.java
@@ -1,2 +1,14 @@
-package com.gamebacket.vercel.app.dto;public class AuthenticationResponse {
+package com.gamebacket.vercel.app.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class AuthenticationResponse {
+    private String token;
 }
