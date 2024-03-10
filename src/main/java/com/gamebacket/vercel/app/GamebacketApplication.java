9706d6f05c6cdaa75dc3367a61817751ffb45d5f
Mdsoln <muddyfakih98@gmail.com>
2024-03-10 22:48:33 +0300

    annotating the main application to enable automatic UI refresh querying

--- src/main/java/com/gamebacket/vercel/app/GamebacketApplication.java
+++ src/main/java/com/gamebacket/vercel/app/GamebacketApplication.java
@@ -2,8 +2,10 @@ package com.gamebacket.vercel.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class GamebacketApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GamebacketApplication.class, args);
