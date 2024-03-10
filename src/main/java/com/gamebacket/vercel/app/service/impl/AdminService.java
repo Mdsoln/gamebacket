b10ee02631518c508f51d7c4b2179045a772ffd5
Mdsoln <muddyfakih98@gmail.com>
2024-03-10 23:08:12 +0300

    implementing methods for finding total sales per day

--- src/main/java/com/gamebacket/vercel/app/service/impl/AdminService.java
+++ src/main/java/com/gamebacket/vercel/app/service/impl/AdminService.java
@@ -1,19 +1,18 @@
 package com.gamebacket.vercel.app.service.impl;
 
 import com.gamebacket.vercel.app.constants.Status;
+import com.gamebacket.vercel.app.dto.SalesReport;
 import com.gamebacket.vercel.app.entity.Accessories;
 import com.gamebacket.vercel.app.entity.Games;
 import com.gamebacket.vercel.app.entity.Order;
 import com.gamebacket.vercel.app.entity.OrderStatus;
 import com.gamebacket.vercel.app.exc.EmptyOrNullValueStorageException;
 import com.gamebacket.vercel.app.exc.SearchExceptions;
-import com.gamebacket.vercel.app.repo.AccessoriesRepo;
-import com.gamebacket.vercel.app.repo.GameRepo;
-import com.gamebacket.vercel.app.repo.OrderRepo;
-import com.gamebacket.vercel.app.repo.OrderStatusRepo;
+import com.gamebacket.vercel.app.repo.*;
 import com.gamebacket.vercel.app.service.inter.AdminInterface;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataAccessException;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
@@ -24,7 +23,9 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -33,6 +34,7 @@ public class AdminService implements AdminInterface {
     private final AccessoriesRepo accessoriesRepo;
     private final OrderRepo orderRepo;
     private final OrderStatusRepo orderStatusRepo;
+    private final OrderItemRepo orderItemRepo;
 
     @Override
     public void publishNewGame(String gameTitle, String gamePlatforms, float actualPrice,
@@ -100,8 +102,28 @@ public class AdminService implements AdminInterface {
         }
     }
 
+    //filtering sales and display sales based on current day
+    @Override
+    public List<SalesReport> findTodaysSales() {
+        List<SalesReport> dailyReport = orderItemRepo.findTotalSalesPerDay();
+        LocalDate today = LocalDate.now();
+
+        return dailyReport.stream()
+                .filter(report-> report.getDateCreated().equals(today))
+                .collect(Collectors.toList());
+    }
+
+
+     @Scheduled(fixedRate = 1000 * 60 * 60, initialDelay = 1000) // Schedule every hour with a 1-second initial delay
+    public void updateTodaysSales() {
+    List<SalesReport> todaysSalesReport = findTodaysSales();
+    // Update your UI logic here using todaysSalesReport data
+         //using react or angular framework to update the ui
+         //sometimes websocket etc.
+    }
+
     private String storeImagePathToDatabase(MultipartFile image) throws IOException {
-        String uploadDirectory = "C:/Users/rojo/Documents/SolnCodes/gamebacket/images";
+        String uploadDirectory = "/dir/uploads";
         String imageName = StringUtils.cleanPath(Objects.requireNonNull(image.getOriginalFilename()));
 
 
--- src/main/java/com/gamebacket/vercel/app/service/inter/AdminInterface.java
+++ src/main/java/com/gamebacket/vercel/app/service/inter/AdminInterface.java
@@ -1,8 +1,10 @@
 package com.gamebacket.vercel.app.service.inter;
 
+import com.gamebacket.vercel.app.dto.SalesReport;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public interface AdminInterface {
     void publishNewGame(String gameTitle, String gamePlatforms, float actualPrice, float discountPrice, int gamePlaytime, int gameAge, String genre, String website, String tags, String aboutGame, LocalDate releaseDate, int gameQuantity, String requirements, MultipartFile imagePath);
@@ -10,4 +12,7 @@ public interface AdminInterface {
     void publishAccessories(String productName, String category, String description, float price, int quantity, MultipartFile imagePath);
 
     void confirmCompleteOrders(String orderNo);
+
+    List<SalesReport> findTodaysSales();
+
 }
