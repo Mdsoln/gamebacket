44902a6d8828ab7384c83cd60a30697d9afe7f03
Mdsoln <muddyfakih98@gmail.com>
2024-03-10 23:01:28 +0300

    payload response for total sales per day and per week

--- src/main/java/com/gamebacket/vercel/app/dto/SalesReport.java
+++ src/main/java/com/gamebacket/vercel/app/dto/SalesReport.java
@@ -1,4 +1,17 @@
 package com.gamebacket.vercel.app.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class SalesReport {
+    private Double totalSales;
+    private LocalDate dateCreated; // For daily report
+    private Integer year;
+    private Integer week;
 }
